79ffdabc61919eb2823566ce7d126be17373aace
savakazakov <savakazakov@gmail.com>
2022-06-14 15:24:17 +0100

    Initial commit

---
+++ java/something.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class something
+{
+    public static void main(String[] args)
+    {
+        Scanner myObj = new Scanner(System.in);
+
+        int kPeople = myObj.nextInt();
+        int nPlanes = myObj.nextInt();
+        int sPlanesFromSheet = myObj.nextInt();
+        int pPacks =  myObj.nextInt();
+
+        double sheetsNeeded = kPeople * Math.ceil((double) nPlanes / sPlanesFromSheet);
+
+        int packsNeeded = (int) Math.ceil(sheetsNeeded/pPacks);
+
+        System.out.println(packsNeeded);
+    }
+
+}
