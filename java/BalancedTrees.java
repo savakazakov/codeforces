65b47f6dec5d9b0cbaa627dd002861306cdbfeb0
savakazakov <savakazakov@gmail.com>
2022-06-18 18:28:21 +0100

    BalancedTrees Time Exceeded

--- java/BalancedTrees.java
+++ java/BalancedTrees.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -8,16 +9,18 @@ public class BalancedTrees
     {
         try(Scanner myObj = new Scanner(System.in))
         {
-            int tests = myObj.nextInt(), n;
-            List<Integer> tree = new ArrayList<>();
+            int tests = myObj.nextInt(), n, count = 0;
+            List<Integer> tree = new ArrayList<>(), subTree = new ArrayList<>();
             String colours;
 
-            System.out.println(",,,,,,,,,,,,");
+            // System.out.println(",,,,,,,,,,,,");
 
             for(int i = 0; i < tests; i++)
             {
+                // Get the input.
                 // Number of vertices including the root.
                 n = myObj.nextInt();
+                count = 0;
 
                 tree.add(0);
 
@@ -26,15 +29,59 @@ public class BalancedTrees
                     tree.add(myObj.nextInt());
                 }
 
-                
                 colours = myObj.next();
-                
-                System.out.println(tree);
-                System.out.println(colours);
 
-                
+                // Main algorithm.
+                for(int j = 1; j < tree.size(); j++)
+                {
+                    fillTree(tree, subTree, j);
+                    if(checkBalanced(subTree, colours))
+                        count++;
+                    subTree.clear();
+                }
+
+                // Reset the ADT.
                 tree.clear();
+
+                System.out.println(count);
             }
         }
     }
+
+    public static void fillTree(List<Integer> tree, List<Integer> subTree, int root)
+    {
+        subTree.add(root);
+
+        int index = tree.indexOf(root);
+
+        while(index > -1 && index < tree.size())
+        {
+            if(tree.get(index) == root)
+                fillTree(tree, subTree, index + 1);
+            index++;
+        }
+    }
+
+    public static boolean checkBalanced(List<Integer> subTree, String colours)
+    {
+        int bal = 0;
+
+        for(int n : subTree)
+        {
+            if(colours.charAt(n - 1) == 'B')
+            {
+                bal--;
+            }
+            else
+                bal++;
+        }
+
+        return bal == 0;
+    }
 }
+/* 
+1
+12
+1 2 2 2 5 6 7 8 5 4 9
+WBBWBWWBBBBW
+ */
