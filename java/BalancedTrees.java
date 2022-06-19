76cd32238f5e719cf3c6858c60768bceed2a5a9c
savakazakov <savakazakov@gmail.com>
2022-06-19 23:33:38 +0100

    Time exceeded on test 12 - BalancedTrees.java

--- java/BalancedTrees.java
+++ java/BalancedTrees.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class BalancedTrees
@@ -10,10 +12,11 @@ public class BalancedTrees
         try(Scanner myObj = new Scanner(System.in))
         {
             int tests = myObj.nextInt(), n, count = 0;
-            List<Integer> tree = new ArrayList<>(), subTree = new ArrayList<>();
-            String colours;
 
-            // System.out.println(",,,,,,,,,,,,");
+            List<Integer> tree = new ArrayList<>();
+            Map<Integer, List<Integer>> subTrees = new HashMap<>();
+
+            String colours;
 
             for(int i = 0; i < tests; i++)
             {
@@ -32,36 +35,69 @@ public class BalancedTrees
                 colours = myObj.next();
 
                 // Main algorithm.
-                for(int j = 1; j < tree.size(); j++)
+                for(int j = tree.size(); j > 0; j--)
                 {
-                    fillTree(tree, subTree, j);
-                    if(checkBalanced(subTree, colours))
-                        count++;
-                    subTree.clear();
+                    // If the value at index (j - 1) is a root of a sub tree.
+                    if(subTrees.containsKey(tree.get(j - 1)))
+                    {
+                        // Now if there is already a tree with root j.
+                        if(subTrees.containsKey(j))
+                        {
+                            if(checkBalanced(subTrees.get(j), colours))
+                            {
+                                // System.out.println(subTrees.get(j));
+                                count++;
+                            }
+                            // Merge the trees.
+                            subTrees.get(tree.get(j - 1)).addAll(subTrees.get(j));
+                            subTrees.remove(j);
+                        }
+                        else
+                        {
+                            // Add j to the sub tree.
+                            subTrees.get(tree.get(j - 1)).add(j);
+                        }
+                    }
+                    // If there is a sub tree with root j.
+                    else if(subTrees.containsKey(j))
+                    {
+                        if(checkBalanced(subTrees.get(j), colours))
+                        {
+                            // System.out.println(subTrees.get(j));
+                            count++;
+                        }
+
+                        subTrees.get(j).add(0, tree.get(j - 1));
+                        subTrees.put(tree.get(j - 1), subTrees.get(j));
+                        subTrees.remove(j);
+                    }
+                    else
+                    {
+                        // Create a new entry.
+                        subTrees.put(tree.get(j - 1), new ArrayList<>(Arrays.asList(tree.get(j - 1), j)));
+                    }
                 }
 
-                // Reset the ADT.
+                // Reset the ADTs.
                 tree.clear();
+                subTrees.clear();
 
                 System.out.println(count);
             }
         }
     }
 
-    public static void fillTree(List<Integer> tree, List<Integer> subTree, int root)
-    {
-        subTree.add(root);
-
-        int index = tree.indexOf(root);
-
-        while(index > -1 && index < tree.size())
-        {
-            if(tree.get(index) == root)
-                fillTree(tree, subTree, index + 1);
-            index++;
-        }
-    }
-
+    /**
+     * Checks if the specified tree is balanced,
+     * i.e. number of black and white nodes are equal
+     * 
+     * @param subTree
+     *            is the specified tree.
+     * @param colours
+     *            is a String which specifies the colours
+     *            of the whole test case.
+     * @return true is balance and false otherwise.
+     */
     public static boolean checkBalanced(List<Integer> subTree, String colours)
     {
         int bal = 0;
@@ -81,7 +117,12 @@ public class BalancedTrees
 }
 /* 
 1
-12
-1 2 2 2 5 6 7 8 5 4 9
-WBBWBWWBBBBW
- */
+16
+1 1 3 4 3 5 7 5 7 10 10 12 6 4 14
+BBWBWBWBWWBWWWWB
+
+1
+7
+1 1 2 3 3 5
+WBBWWBW
+*/
