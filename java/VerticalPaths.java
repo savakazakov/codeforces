898299fe03e57d62e60a256e0651b2478ef23098
savakazakov <savakazakov@gmail.com>
2022-07-02 15:25:49 +0100

    Almost working VerticalPaths.java

--- java/VerticalPaths.java
+++ java/VerticalPaths.java
@@ -1,9 +1,11 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
+import java.time.temporal.Temporal;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+// import java.util.Collections;
+// import java.util.Iterator;
+// import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * This problem's task can be found at : https://codeforces.com/problemset/problem/1675/D
@@ -15,12 +17,9 @@ public class VerticalPaths
     {
         try(Scanner myObj = new Scanner(System.in))
         {
-            int tests = myObj.nextInt(), n;
+            int tests = myObj.nextInt(), n, temp;
 
-            List<List<Integer>> tree = /* Collections.synchronizedList( */new ArrayList<>()/* ) */;
-
-            // AbstractMap.SimpleEntry<Integer, Integer> pair = new AbstractMap.SimpleEntry<>(pair);
-            // Map<Long, Long> numbers = new TreeMap<>();
+            CopyOnWriteArrayList<CopyOnWriteArrayList<Integer>> tree = new CopyOnWriteArrayList<>();
 
             for(int i = 0; i < tests; i++)
             {
@@ -29,44 +28,75 @@ public class VerticalPaths
                 // Get the input.
                 for(int j = 0; j < n; j++)
                 {
-                    tree.add(/* Collections.synchronizedList( */new ArrayList<>(Arrays.asList(j + 1, myObj.nextInt())))/* ) */;
+                    temp = myObj.nextInt();
+
+                    /* if(temp != j + 1) */
+                        tree.add(new CopyOnWriteArrayList<>(Arrays.asList(j + 1, temp)));
                 }
 
                 findVerticalPaths(tree);
-                System.out.println(tree);
-                System.out.println("check");
+
+                // Output the result.
+                System.out.println(tree.size());
+
+                for(CopyOnWriteArrayList<Integer> l : tree)
+                {
+                    System.out.println(l.size() + " size of branch");
+                    
+                    for(int j = l.size() - 1; j >= 0; j--)
+                    {
+                        System.out.println(l.get(j));
+                    }
+                }
+
                 tree.clear();
             }
         }
     }
 
-    public static /* synchronized */ void findVerticalPaths(List<List<Integer>> tree)
+    public static /* synchronized */ void findVerticalPaths(CopyOnWriteArrayList<CopyOnWriteArrayList<Integer>> tree)
     {
         int parent = -1;
+        // ArrayList<Integer> l;
 
-        Iterator<List<Integer>> itr = tree.iterator();
+        // Iterator<List<Integer>> treeItr = tree.iterator();
         
-        for(List<Integer> l : tree)
+        // while(treeItr.hasNext())
+        // {
+        //     l = (ArrayList<Integer>) treeItr.next();
+        //     parent = l.get(l.size() -1);
+
+        //     treeItr
+
+        //     // treeItr.forEachRemaining(action);
+        // }
+
+        for(CopyOnWriteArrayList<Integer> l : tree)
         {
+            if(l.size() == 0)
+                break;
+
             // Get the end of the path.
             parent = l.get(l.size() - 1);
 
             // Scan the remaining paths for that parent node.
-            for(List<Integer> k : tree)
+            for(CopyOnWriteArrayList<Integer> k : tree)
             {
                 // Merge the paths.
                 if(k.get(0) == parent && !k.equals(l))
                 {
                     k.remove(0);
-                    l.addAll(k);
+                    // l.addAll(k);
                     tree.remove(k);
-
-                    // Clear all the nodes which have the same parent.
-                    for(List<Integer> t : tree)
+                    if(l.addAll(k))
                     {
-                        if(t.get(t.size() - 1) == parent)
+                        // Clear all the nodes which have the same parent.
+                        for(CopyOnWriteArrayList<Integer> t : tree)
                         {
-                            t.remove((Object) parent);
+                            if(t.get(t.size() - 1) == parent/*  && !t.equals(l) */)
+                            {
+                                t.remove((Object) parent);
+                            }
                         }
                     }
 
@@ -81,4 +111,24 @@ public class VerticalPaths
 1
 5
 3 1 3 3 1
+
+1
+4
+1 1 4 1
+
+1
+7
+1 1 2 3 4 5 6
+
+1
+1
+1
+
+1
+6
+4 4 4 4 1 2
+
+1
+4
+2 2 2 2
  */
